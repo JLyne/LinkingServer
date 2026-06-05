7de023c2ff08a17de3ca65cd84fb3a7d89eaa478
James Lyne <jim+github@not-null.co.uk>
2026-06-05 19:12:10 +0100

    26.2-pre4

--- linkingserver/versions/__init__.py
+++ linkingserver/versions/__init__.py
@@ -10,3 +10,4 @@ from .version_1_21_7 import Version_1_21_7
 from .version_1_21_9 import Version_1_21_9
 from .version_1_21_11 import Version_1_21_11
 from .version_26_1 import Version_26_1
+from .version_26_2 import Version_26_2
---
+++ linkingserver/versions/version_26_2.py
@@ -0,0 +1,36 @@
+from quarry.types.namespaced_key import NamespacedKey
+
+from linkingserver.protocol import Protocol
+from linkingserver.versions import Version_26_1
+
+
+class Version_26_2(Version_26_1):
+    protocol_version = 1073742142
+
+    def __init__(self, protocol: Protocol, bedrock: bool):
+        super(Version_26_1, self).__init__(protocol, bedrock)
+
+    def send_join_game(self):
+        dimension_registry = self.protocol.data_packs.get_registry(NamespacedKey.minecraft('dimension_type'))
+        id = list(dimension_registry).index(NamespacedKey.minecraft('overworld'))
+
+        self.protocol.send_packet("login",
+                                  self.protocol.buff_type.pack("i?", 830, False),
+                                  self.protocol.buff_type.pack_varint(2),
+                                  self.protocol.buff_type.pack_string("rtgame:linking"),
+                                  self.protocol.buff_type.pack_string("rtgame:reset"),
+                                  self.protocol.buff_type.pack_varint(0),
+                                  self.protocol.buff_type.pack_varint(7),
+                                  self.protocol.buff_type.pack_varint(0),
+                                  self.protocol.buff_type.pack("???", False, True, False),
+                                  self.protocol.buff_type.pack_varint(id),
+                                  self.protocol.buff_type.pack_string("rtgame:linking"),
+                                  self.protocol.buff_type.pack("qBb??", 0, 1, 1, False, False),
+                                  self.protocol.buff_type.pack("?", False),
+                                  self.protocol.buff_type.pack_varint(0),
+                                  self.protocol.buff_type.pack_varint(64),
+                                  self.protocol.buff_type.pack("?", True), # Enable online mode
+                                  self.protocol.buff_type.pack("?", False))
+
+    def get_written_book_id(self):
+        return 1251
