2eaf874b064919d0a1510c594367a45085096b4e
James Lyne <jim+github@not-null.co.uk>
2026-06-11 20:38:54 +0100

    26.2-rc1

--- linkingserver/versions/version_26_2.py
+++ linkingserver/versions/version_26_2.py
@@ -5,7 +5,7 @@ from linkingserver.versions import Version_26_1
 
 
 class Version_26_2(Version_26_1):
-    protocol_version = 1073742142
+    protocol_version = 1073742145
 
     def __init__(self, protocol: Protocol, bedrock: bool):
         super(Version_26_1, self).__init__(protocol, bedrock)
